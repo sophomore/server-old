b23b23828a8114baf88a35e4001fce46fb1a7107
JinYong Kim <jinyong@jadekim.kr>
2015-09-12 16:10:19 +0900

    no message

--- order_manager.py
+++ order_manager.py
@@ -8,7 +8,7 @@ def add_order(time, totalprice, ordermenus_info):
     order = Order(time, totalprice)
 
     for ordermenu_info in ordermenus_info:
-        order.ordermenus.append(OrderMenu(Menu.query.filter_by(id=ordermenu_info['id']).first(), ordermenu_info['pay'], order, ordermenu_info['curry'], ordermenu_info['double']))
+        db.add(OrderMenu(Menu.query.filter_by(id=ordermenu_info['id']).first(), ordermenu_info['pay'], order, ordermenu_info['curry'], ordermenu_info['double']))
 
     db.add(order)
     db.commit()
