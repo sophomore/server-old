397feeeef1041682e4012f79cf9039fe1b1d33f8
JinYong Kim <jinyong@jadekim.kr>
2015-09-15 22:24:43 +0900

    no message

--- order_manager.py
+++ order_manager.py
@@ -50,7 +50,7 @@ def search(startDate, endDate, ordermenus, pay):
     print(startDate, endDate, ordermenus, pay)
     orders = []
     for ordermenu in ordermenus:
-        orders.append(Order.query.join(Order.ordermenus, aliased=True).filter_by(menu_id=ordermenu, pay=pay).filter(startDate <= Order.time, Order.time <= endDate).all())
+        orders += Order.query.join(Order.ordermenus, aliased=True).filter_by(menu_id=ordermenu, pay=pay).filter(startDate <= Order.time, Order.time <= endDate).all()
 
     print(orders)
 
