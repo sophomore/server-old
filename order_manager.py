1bd29f2b47b52ea423346e36fcaad8cca2a534ab
JinYong Kim <jinyong@jadekim.kr>
2015-09-15 21:28:01 +0900

    no message

--- order_manager.py
+++ order_manager.py
@@ -47,6 +47,8 @@ def get_one_dict(id):
 
 
 def search(startDate, endDate, ordermenus, pay):
-    result = db.query(Order).filter(OrderMenu.menu_id == ordermenus[0], OrderMenu.pay == pay).all()
-    for order in result:
+    result = db.query(Order, OrderMenu).filter(OrderMenu.menu_id == ordermenus[0], OrderMenu.pay == pay).all()
+    for order, ordermenu in result:
         print(order.convert_dict())
+        print(ordermenu.convert_dict())
+        print()
