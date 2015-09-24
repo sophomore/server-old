d9528a9364d64f5fd4cf1c7a10d98b4f87e8bf84
OhYongTaek <ka123ak1@gmail.com>
2015-09-24 21:17:50 +0900

    no message

--- statistic.py
+++ statistic.py
@@ -165,7 +165,7 @@ def unit_menu_sum(startDate, endDate, menus, unit):
     while currentDate<=endDate:
         temp = createResultDic(temp,unit,currentDate)
         if unit == 1:
-            orders = db.query(Order).filter(startDate <= Order.time, Order.time <= currentDate.replace(hour =23,minute = 59,second = 59)).all()
+            orders = db.query(Order).filter(startDate <= Order.time, Order.time <= endDate.replace(hour =23,minute = 59,second = 59)).all()
             for order in orders:
                 for ordermenu in order.ordermenus:
                     if ordermenu.menu_id in temp[order.time.hour.real]:
