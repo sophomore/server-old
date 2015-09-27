2c0c52d512b5c33f35f3072fa36148ad8b1e707f
OhYongTaek <ka123ak1@gmail.com>
2015-09-27 23:30:12 +0900

    no message

--- statistic.py
+++ statistic.py
@@ -174,8 +174,7 @@ def unit_menu_sum(startDate, endDate, menus, unit):
             orders = db.query(Order).filter(startDate <= Order.time, Order.time <= endDate.replace(hour =23,minute = 59,second = 59)).all()
             for order in orders:
                 for ordermenu in order.ordermenus:
-
-                    if ordermenu.menu_id in temp[order.time.hour.real]:
+                    if ordermenu.menu_id in temp[order.time.hour.real]['menu']:
                         print(ordermenu.menu_id)
                         print("@@")
                         temp[order.time.hour.real]['menu'][ordermenu.menu_id]['price'] += ordermenu.totalprice
