2b68d9897119f75c0227b4c4ed928a66f65e3858
OhYongTaek <ka123ak1@gmail.com>
2015-10-13 05:30:02 +0900

    no message

--- order_manager.py
+++ order_manager.py
@@ -12,7 +12,7 @@ def add_order(time, totalprice, ordermenus_info):
     db.commit()
 
     for ordermenu_info in ordermenus_info:
-        menu =  Menu.query.filter_by(id=ordermenu_info['id']).first()
+        menu =  Menu.query.filter_by(id=ordermenu_info['menu_id']).first()
         print(menu)
         ordermenu = OrderMenu(menu=menu, order=order, pay=ordermenu_info['pay'], curry=ordermenu_info['curry'], twice=ordermenu_info['twice'],takeout=ordermenu_info['takeout'])
         db.add(ordermenu)
