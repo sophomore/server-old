f1736949a54a60d79051f5435c1a8b0cc98bf710
Sun Yeong <vmffkxlgnqh1@gmail.com>
2015-10-14 00:45:14 +0900

    no message

--- util.py
+++ util.py
@@ -66,7 +66,7 @@ def input():
                         with open('notmatchedmenu.csv','wb') as f:
                             if bef in ms:
                                 ordermenu = OrderMenu(menu=ms[bef],order=order, pay=pay,curry=a(count_curry),
-                                twice=a(count_twice),takeout=takeout(str(row[3].value))
+                                twice=a(count_twice),takeout=takeout(str(row[3].value)))
                                 count_twice = count_twice - 1
                                 count_curry = count_curry - 1
                                 db.add(ordermenu)
@@ -76,7 +76,7 @@ def input():
 
                 else:
                     ordermenu = OrderMenu(menu=ms[o],order=order,pay=pay,curry=a(count_curry),
-                    twice=(count_twice),takeout=takeout(str(row[3].value)) 
+                    twice=(count_twice),takeout=takeout(str(row[3].value)))
                     count_twice = count_twice - 1
                     count_curry = count_curry - 1 
                     db.add(ordermenu)
