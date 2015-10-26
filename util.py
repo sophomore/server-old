4669aea0d238178961ae1ef392bb043cf9c65bb7
Sun Yeong <vmffkxlgnqh1@gmail.com>
2015-10-26 22:37:24 +0900

    no message

--- util.py
+++ util.py
@@ -30,17 +30,17 @@ def print_statement(orders,ordertime):
     for o in order:
         orderstring +=u''+o+'\x09'+str(order[o])+'\x09'+str(price[o])+'\x09'+str(order[o]*price[o])+'\n'
 
-    output = ''
-    output +=u'\n상 호 명: 송호성 쉐프의 돈까스\n'
+    output =u'\x1Bd\x11\x08\x00'
+    output +=u'상 호 명: 송호성 쉐프의 돈까스\n'
     output +=u'등록번호: 134-31-16828\n'
     output +=u'대   표: 송호성\n'
     output +=u'전화번호: 031-480-4595\n'
     output +=u'주   소: 경기 안산시 상록구 사동 1165번지\n\n'
     output +=u'주문:'+ordertime+'\n'
     output +=u'---------------------------------\n'
-    output +=u'상 품 명'.center(6)+'수량'.center(2)+'단가'.center(5)+'금 액'.center(6)+'\n'
-    output +=u''+orderstring+'\n\n'
-    output +=u'---------------------------------\n\n'
+    output +=u'상 품 명'.center(6)+'  수량'.center(2)+'단가'.center(5)+'금 액'.center(6)+'\n'
+    output +=u''+orderstring
+    output +=u'---------------------------------\n\n\n\n'
     output +=u'\x1bm'
     f1 = open('./test','w+',encoding="euc-kr")
     print(output,file = f1)
