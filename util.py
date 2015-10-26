03fe46cfefd164aab7195e73bd25a7fcb75bce30
Sun Yeong <vmffkxlgnqh1@gmail.com>
2015-10-26 22:43:00 +0900

    no message

--- util.py
+++ util.py
@@ -30,7 +30,7 @@ def print_statement(orders,ordertime):
     for o in order:
         orderstring +=u''+o+'\x09'+str(order[o])+'\x09'+str(price[o])+'\x09'+str(order[o]*price[o])+'\n'
 
-    output =u'\x1Bd\x13\x10\x00'
+    output =u'\x1Bd\x1e\x\x00'
     output +=u'상 호 명: 송호성 쉐프의 돈까스\n'
     output +=u'등록번호: 134-31-16828\n'
     output +=u'대   표: 송호성\n'
