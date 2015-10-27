cbef6f9a971715cee24fdcd03754c2053cbf3e0f
Sun Yeong <vmffkxlgnqh1@gmail.com>
2015-10-28 00:10:28 +0900

    no message

--- util.py
+++ util.py
@@ -66,7 +66,7 @@ def print_statement(ordermenus,time):
     string +=u'메    뉴    수량\n'
     string +=u'-------------------------------------\n'
     for key in order:
-        if order[key]-curry[key]-twice[key]) >0:
+        if order[key]-curry[key]-twice[key] >0:
             string += u''+key+'      '+str(order[key]-curry[key]-twice[key]+ct[key])+'\n'
             string += u'\x09일반'
         if ct[key]>0:
