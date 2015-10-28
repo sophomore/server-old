7eb8f090cac98fd4bc886c82ff4d52f43e69b9fa
OhYongTaek <ka123ak1@gmail.com>
2015-10-28 18:07:26 +0900

    no message

--- util.py
+++ util.py
@@ -132,19 +132,19 @@ def print_receipt(orders):
     orderstring +=u'-------------------------------------\n'
     orderstring +=u'    합계\x09\x09\x09'+str(summ)+'\n'
 
-
-    output =u'상 호 명: 송호성 쉐프의 돈까스\n'
+    output = u'\n\n\n\n\n\n'
+    output += u'\x1b\x44\x11\x16\x1d\x00'
+    output +=u'상 호 명: 송호성 쉐프의 돈까스\n'
     output +=u'등록번호: 134-31-16828\n'
     output +=u'대   표: 송호성\n'
     output +=u'전화번호: 031-480-4595\n'
     output +=u'주   소: 경기 안산시 상록구 사동 1165번지\n\n'
     output +=u'주문:'+time+'\n'
-    output += u'\x1b\x44\x11\x16\x1d\x00'
     output +=u'-------------------------------------\n'
     output +=u'    상 품 명'.center(6)+'   수량'.center(2)+'  단가'.center(5)+'   금 액'.center(6)+'\n'
     output +=u'-------------------------------------\n'
     output +=u''+orderstring
-    output +=u'-------------------------------------\n\n'
+    output +=u'-------------------------------------\n\n\n\n\n'
     output +=u'\x1bm'
     f1 = open('./receipt','w+',encoding="euc-kr")
     print(output,file = f1)
