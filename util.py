26040d5024fa05666d3e490cb45f351dea619606
OhYongTaek <ka123ak1@gmail.com>
2015-10-28 17:15:05 +0900

    no message

--- util.py
+++ util.py
@@ -133,7 +133,7 @@ def print_receipt(orders):
     orderstring +=u'    합계\x09\x09\x09'+str(summ)+'\n'
 
 
-    output = u'\x1b\x44\x11\x16\x1d\x00\n'
+    output = u'\x1b\x44\x11\x16\x1d\x00\n\n\n\n'
     output +=u'상 호 명: 송호성 쉐프의 돈까스\n'
     output +=u'등록번호: 134-31-16828\n'
     output +=u'대   표: 송호성\n'
