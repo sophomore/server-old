66f5586705bab8b384fe378f307e0156b94ccfdb
Sun Yeong <vmffkxlgnqh1@gmail.com>
2015-10-30 02:23:32 +0900

    no message

--- util.py
+++ util.py
@@ -88,9 +88,8 @@ def print_statement(ordermenus,time):
         if t_twice[key]>0:
             string += u'\x1d\x21\x01'+key+'\x09\x09'+str(t_curry[key])+'\n\x1d\x21\x00'
             string += u'  ㄴ  곱\n\n'
-    outstring = u'                                          '
-    outstring += u'\x1B\x44\x12\x00'
-    outstring +=u'================전     표================\n'
+    outstring = u'\x1B\x44\x12\x00'
+    outstring +=u'================전     표================\n\n\n\n'
     outstring +=u'주문:'+time1+'\n'
     outstring +=u'----------------------------------------\n'
     outstring +=u'메    뉴\x09\x09    수량\n'
@@ -99,8 +98,8 @@ def print_statement(ordermenus,time):
     outstring +=u'----------------------------------------\n\n\n\n\n\n'
     outstring += u'\x1bm'
     f2 = open('./statement','w+',encoding="euc-kr")
-    # string = 
-    # print(string,file = f2)
+    string = u'                                          '
+    print(string,file = f2)
     print(outstring)
     print(outstring,file = f2)
     f2.close()
