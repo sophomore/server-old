5f4208b93b335f6ffa4227afe343db6aade04ba5
Sun Yeong <vmffkxlgnqh1@gmail.com>
2015-10-30 01:11:15 +0900

    no message

--- util.py
+++ util.py
@@ -18,7 +18,7 @@ def get_menus():
 
 def print_statement(ordermenus,time):
     time1 = time.strftime('%Y-%m-%d %H:%M:%S')
-    string =u''+time1
+    string =u''+time1+'\x1bm'
     f = open('./statement','w+',encoding = "euc-kr")
     print(string,file =f)
     f.close()
