301d8fa23eb9b03f1d8195ed6fbf5a2d4e187270
Sun Yeong <vmffkxlgnqh1@gmail.com>
2015-10-30 02:19:55 +0900

    no message

--- util.py
+++ util.py
@@ -89,7 +89,7 @@ def print_statement(ordermenus,time):
             string += u'\x1d\x21\x01'+key+'\x09\x09'+str(t_curry[key])+'\n\x1d\x21\x00'
             string += u'  ㄴ  곱\n\n'
     outstring = u'\x1B\x44\x12\x00'
-    outstring +=u'================전     표================\n\n'
+    outstring +=u'================전     표================\n\n\n\n'
     outstring +=u'주문:'+time1+'\n'
     outstring +=u'----------------------------------------\n'
     outstring +=u'메    뉴\x09\x09    수량\n'
@@ -98,7 +98,7 @@ def print_statement(ordermenus,time):
     outstring +=u'----------------------------------------\n\n\n\n\n\n'
     outstring += u'\x1bm'
     f2 = open('./statement','w+',encoding="euc-kr")
-    string = u'   \n'
+    string = u'                                        '
     print(string,file = f2)
     print(outstring)
     print(outstring,file = f2)
