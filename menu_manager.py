700062d209714d4e5a133dff7f67cab55f5af7a1
OhYongTaek <ka123ak1@gmail.com>
2015-10-29 22:52:28 +0900

    no message

--- menu_manager.py
+++ menu_manager.py
@@ -45,7 +45,8 @@ def get_all_dict():
             if i['name'] == menus[menu].name:
                 check = False
         if check:
-            print(menus[menu]+"!!$@")
+            print('123')
+            print(menus[menu])
             result.append(menus[menu].convert_dict())
     return result
 
