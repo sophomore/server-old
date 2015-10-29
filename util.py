a2c40f0394f41e91d81b8cb8617db3c7cc2e7342
Sun Yeong <vmffkxlgnqh1@gmail.com>
2015-10-30 04:16:31 +0900

    no message

--- util.py
+++ util.py
@@ -145,6 +145,7 @@ def print_receipt(orders):
     orderstring +=u'-----------------------------------------\n'
     orderstring +=u'\x1b\x61\x02합계 : '+str(summ)+'     \n\x1b\x61\x00'
     output = u'                                          \n'
+    output += u'                                          \n'
     output +=u'상 호 명: 송호성 쉐프의 돈까스\n'
     output +=u'등록번호: 134-31-16828\n'
     output +=u'대    표: 송호성\n'
