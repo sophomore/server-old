709f0eb7b048845c708dee88c49ecc44f1939cec
OhYongTaek <ka123ak1@gmail.com>
2015-11-03 13:06:03 +0900

    no message

--- dearsong.py
+++ dearsong.py
@@ -195,7 +195,7 @@ if __name__ == '__main__':
     import logging
     from logging.handlers import RotatingFileHandler
     formatter = logging.Formatter("[%(asctime)s] {%(pathname)s:%(lineno)d} %(levelname)s - %(message)s")
-    handler = RotatingFileHandler("foo.log", maxBytes=10000000, backupCount=5)
+    handler = RotatingFileHandler("/home/song/foo.log", maxBytes=10000000, backupCount=5)
     handler.setLevel(logging.DEBUG)
     handler.setFormatter(formatter)
     app.logger.addHandler(handler)
